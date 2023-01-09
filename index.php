6094f7d98c75bf3995d2522db2e60d9c10404796
Yashar <Yashar.talat@gmail.com>
2023-01-09 18:36:45 +0330

    MainPage

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="style.css">
+    <title>LOGIN</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
-    <form action="">
+    <form action="login.php" method="post">
         <h2>LOGIN</h2>
+        <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
         <label>User Name</label>
-        <input type="text" name="uname" placeholder="User Name">
+        <input type="text" name="uname" placeholder="User Name"><br>
 
-        <label>Password</label>
-        <input type="password" name="password" placeholder="Password">
+        <label>User Name</label>
+        <input type="password" name="password" placeholder="Password"><br>
+
         <button type="submit">Login</button>
     </form>
-
 </body>
 
 </html>
