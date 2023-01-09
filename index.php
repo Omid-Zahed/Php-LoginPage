686ac9a381d20c390bac3b26906e8a828c7052ed
Yashar <Yashar.talat@gmail.com>
2023-01-09 15:25:27 +0330

    createfile-addFeature

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form action="">
+        <h2>LOGIN</h2>
+        <label>User Name</label>
+        <input type="text" name="uname" placeholder="User Name">
+
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Password">
+        <button type="submit">Login</button>
+    </form>
+
+</body>
+
+</html>
